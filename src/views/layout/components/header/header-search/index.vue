8464843186701a07c692a14def91c2a7fab29ff1
wxzYa <2012130329@qq.com>
2024-01-10 21:52:42 +0800

    pc端headsearch

---
+++ src/views/layout/components/header/header-search/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full">
+    <m-search
+      v-model="inputValue"
+    ></m-search>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const inputValue = ref('')
+</script>
+
+<style lang="scss" scoped>
+
+</style>
