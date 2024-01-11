5211b80fb38892a428a0a08fc5e7b76c9b3265f1
wxzYa <2012130329@qq.com>
2024-01-11 22:03:53 +0800

    feat:搜索功能完成

--- src/views/layout/components/header/header-search/index.vue
+++ src/views/layout/components/header/header-search/index.vue
@@ -2,13 +2,31 @@
   <div class="w-full">
     <m-search
       v-model="inputValue"
-    ></m-search>
+      @search="onSearchHandler"
+      @clear="onSearchHandler"
+    >
+    <template #dropdown>
+      <div>111</div>
+    </template>
+  </m-search>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 const inputValue = ref('')
+
+// 搜索的回调事件
+const onSearchHandler = (val) => {
+  inputValue.value = val
+  // if (val) {
+  //   // 保存历史记录
+  //   store.commit('search/addHistory', val)
+  //   console.log('触发 searchText 变化')
+  //   // 触发 searchText 变化
+  //   store.commit('app/changeSearchText', val)
+  // }
+}
 </script>
 
 <style lang="scss" scoped>
